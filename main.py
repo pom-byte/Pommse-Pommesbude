e9e9a255c0fd073678a7c479290af6630f0e5003
Pom <310202197+pom-byte@users.noreply.github.com>
2026-08-02 14:53:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Pommse-Bot ist online!"
+    return "Pommse-Pommesbude ist online und frittiert!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=100000)
 
 def keep_alive():
     t = Thread(target=run)
